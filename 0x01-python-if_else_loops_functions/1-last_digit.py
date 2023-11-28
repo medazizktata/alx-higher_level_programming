237576dcf681e5cf2b117bd91ce896d48fa77499
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-11-28 12:58:34 -0800

    Task 1 v0

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str1 = "Last digit of "
+ld = number mod 1000
+if ld > 5:
+    str2 = " and is greater than 5\n"
+elif ld == 0:
+    str2 = " and is 0\n"
+elif ld < 6 and ld != 0:
+    str2 = " and is less than 6 and not 0\n"
+print(str1 + number + str2 + '\n')
