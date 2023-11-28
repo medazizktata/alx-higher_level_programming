12d77f9d36f5dfb9ea268621e3c6597fa6a512cb
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-11-28 13:04:30 -0800

    Task 1 v3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,4 +9,4 @@ elif ld == 0:
     str2 = " and is 0\n"
 elif ld < 6 and ld != 0:
     str2 = " and is less than 6 and not 0\n"
-print(f"{str1}{number} is {ld} and {str2}\n")
+print(f"{str1}{number} is {ld}{str2}\n")
