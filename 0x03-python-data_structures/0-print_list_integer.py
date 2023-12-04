4452cb1ab90e022d31ac6491327a356f4517cc78
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-04 10:48:13 -0800

    Task 0 v2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{}\n".format(my_list[]))
+    if len(my_list) != 0:
+        for i in range(len(my_list)):
+            print("{}\n".format(my_list[i]))
