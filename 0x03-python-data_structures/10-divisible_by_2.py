448fa3ac998780ff0c43339a11a536b904e28ec0
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-04 12:35:21 -0800

    Task 10 v0

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in range(my_list):
+        if(my_list[i] mod 2 == 0):
+            list[i] = True
+        else:
+            list[i] = False
+    return list
