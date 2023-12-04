05f8fe6d09b4033f98b0cf56c853c32920aab55d
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-04 12:42:03 -0800

    Task 10 v 3

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     lista = []
-    for i in range(my_list):
+    for i in range(len(my_list)):
         lista.append(my_list[i] % 2 == 0)
     return lista
