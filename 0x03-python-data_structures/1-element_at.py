ae56bffd6a4753047c9e82e2b6549bb9b4cbb80c
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-04 10:57:29 -0800

    Task 1v2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0 or len(my_list) <= idx:
-        return 'None'
+        return None
     else:
-        return mylist[idx]
+        return my_list[idx]
