ca5103c45869917168b0b9bfdb6d33e9e15054a7
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-04 11:45:10 -0800

    Task 5 v1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     i = 0
-    while i < len(my_string) and (ord(my_string[i]) = 67 or ord(my_string[i]) = 99):
+    while i < len(my_string) and (ord(my_string[i]) == 67 or ord(my_string[i]) == 99):
         mystring = my_string[:i] + my_string[i + 1:]
         i += 1
