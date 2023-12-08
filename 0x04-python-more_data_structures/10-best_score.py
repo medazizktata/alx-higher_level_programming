1e8bda38fbfa9b9a68adbba96de324d77941bfd7
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-08 14:33:20 -0800

    Task 10 v1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     max = next(iter(a_dictionary.values()))
+    key_max = next(iter(a_dictionary))
     for key, value in a_dictionary.items():
         if value > max:
             max = value
-            k = key
-    return k
+            key_max = key
+    return key_max
