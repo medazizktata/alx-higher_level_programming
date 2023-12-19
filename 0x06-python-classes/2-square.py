5b8f6ecb232d0a09f748a4e8bbb25b440270a9bc
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-19 11:02:47 -0800

    Task 2 v5

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""Define a Square"""
+"""Define a square."""
+
+
 class Square:
+    """ Represent a square."""
     def __init__(self, size=0):
-        """Represent a square
-        args: size=0 """
+        """Initialize a square
+        args: size = 0"""
         if not (isinstance(size, int)):
             raise TypeError("size must be an integer")
         elif size < 0:
