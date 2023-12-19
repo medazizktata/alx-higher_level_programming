8b9afef08fe894679d69b3f1434e43383ada3c26
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-18 16:29:28 -0800

    Task 0 v0

---
+++ 0x05-python_exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print(my_list[count])
+            x += 1
+        except my_list[x] is None:
+            break
