b83d428c86f4d50f6362f3206c8b7b62e85cf418
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-19 10:53:59 -0800

    Task 4 v0

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Define a Square"""
+class Square:
+    """Represent a square"""
+    def __init__(self, size=0):
+        """Initialize a square
+        args: size=0 """
+        if size < 0:
+            raise ValueError("size must be an integer")
+        if not (isinstance(size, int)):
+            raise TypeError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Returns square area
+        args: self"""
+        return self.__size ** 2
+    def size(self, value):
+        """Set size value
+        args: value"""
+        self.__size = value
