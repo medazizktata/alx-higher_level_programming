f3239c3deb4e3767c9af8bfa7ab2635ce71b0cd8
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-19 11:00:52 -0800

    Task 2, 3, 4 v0 corrected

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,10 +5,10 @@ class Square:
     def __init__(self, size=0):
         """Initialize a square
         args: size=0 """
-        if size < 0:
-            raise ValueError("size must be an integer")
         if not (isinstance(size, int)):
-            raise TypeError("size must be >= 0")
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
         """Returns square area
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,16 +5,25 @@ class Square:
     def __init__(self, size=0):
         """Initialize a square
         args: size=0 """
-        if size < 0:
-            raise ValueError("size must be an integer")
         if not (isinstance(size, int)):
-            raise TypeError("size must be >= 0")
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
         """Returns square area
         args: self"""
         return self.__size ** 2
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+    @size.setter
     def size(self, value):
         """Set size value
         args: value"""
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
