04de2b7820f3990b2107b7efab9abd44e7a7b511
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-19 10:36:10 -0800

    TAsk 1 v1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Define a square based on 0"""
+
 class Square:
-    """Class Square
+    """Initialize Square
     Args: size
     """
     def __init__(self, size):
-        self.size = size
+        self.__size = size
