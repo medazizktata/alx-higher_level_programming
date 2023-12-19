b7176beed208ff62199e3aea5642987e058923e0
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-19 10:27:20 -0800

    TAsk 0 v1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Square:
-    arrete
-    surface = arrte ** 2
     def __init__(self, arrete):
         self.arrete = arrete
+    def calculate_area(self):
+        return self.arrete ** 2
