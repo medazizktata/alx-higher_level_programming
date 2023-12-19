56236a3ea3ea61424a3baf6abd125d13ff4f596c
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-18 17:03:27 -0800

    TAsk 0 and 1 update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     if x == 0:
         print()
         return 0
+    count = 0
     try:
-        count = 0
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             count += 1
