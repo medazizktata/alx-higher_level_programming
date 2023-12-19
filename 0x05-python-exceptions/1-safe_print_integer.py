e816b1b4972b295ddc5f2e79b6e858f207aaad89
Mohamed Aziz Ktata <aziiizktata7@gmail.com>
2023-12-18 17:02:10 -0800

    Task 1 v4

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value), end="")
+        print("{:d}".format(value))
         print()
         return True
     except ValueError:
         pass
     except TypeError:
         pass
-    print()
     return False
